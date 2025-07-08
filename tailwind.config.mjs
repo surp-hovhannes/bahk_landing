cd1ec07dedb6b8dd1b17bbafccb10cf59d2b79b3
anahid33 <63259931+anahid33@users.noreply.github.com>
2025-07-07 18:30:45 -0700

    added "Download App" button and Logo to the front page, changed font to Figtree

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,7 +1,11 @@
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Figtree', 'ui-sans-serif', 'system-ui', 'sans-serif'],
+      },
+    },
   },
   plugins: [
     require('@tailwindcss/typography'),
